addcd555b01b403f20258460d2e37d50c46e05b6
Igaguri <igagurimk@gmail.com>
2017-12-16 18:16:12 +0900

    ジャーナルファイルを選別しつつ読み込む

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"os"
-	"regexp"
-	"strings"
 	"time"
+
+	"github.com/IgaguriMK/rescueSummary/journal"
 )
+
+var rescueUpdate = time.Date(2017, 12, 14, 6, 0, 0, 0, time.UTC)
 
 func main() {
 	logfile, err := os.OpenFile("error.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
@@ -19,44 +22,45 @@ func main() {
 	log.SetOutput(logfile)
 	log.Println("----------------")
 
-	for _, file := range journalFiles() {
-		t, err := journalNameToTime(file)
+	forJournals(func(jf journal.JournalFile) {
+		if jf.StartAt().Before(rescueUpdate) {
+			return
+		}
+		r, err := jf.Open()
 		if err != nil {
-			fmt.Println(err)
-			continue
+			log.Fatal(err)
 		}
-		fmt.Println(t, " : ", file)
-	}
+
+		lines, err := readLines(r)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%v\n", lines)
+	})
 }
 
-func journalDir() string {
-	return os.Getenv("USERPROFILE") + `\Saved Games\Frontier Developments\Elite Dangerous`
-}
-
-var journalExpr = regexp.MustCompile(`^Journal\.[0-9]{12}\.[0-9]{2}\.log$`)
-
-func journalFiles() []string {
-	jd := journalDir()
-	file, err := ioutil.ReadDir(jd)
+func forJournals(fn func(journalFile journal.JournalFile)) {
+	files, err := journal.JournalFiles()
 	if err != nil {
-		log.Printf("ジャーナルファイルのディレクトリが開けません\n    %v", err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
-
-	fnames := make([]string, 0)
-	for _, f := range file {
-		if journalExpr.MatchString(f.Name()) {
-			fnames = append(fnames, f.Name())
-		}
+	for _, file := range files {
+		fn(file)
 	}
-
-	return fnames
 }
 
-const journalTime = "060102150405"
+func readLines(r *os.File) ([]string, error) {
+	sc := bufio.NewScanner(r)
 
-func journalNameToTime(name string) (time.Time, error) {
-	name = strings.TrimPrefix(name, "Journal.")
-	name = strings.TrimSuffix(name, ".01.log")
-	return time.Parse(journalTime, name)
+	lines := make([]string, 0)
+	for sc.Scan() {
+		lines = append(lines, sc.Text())
+	}
+
+	if err := sc.Err(); err != nil && err != io.EOF {
+		return lines, err
+	}
+
+	return lines, nil
 }
